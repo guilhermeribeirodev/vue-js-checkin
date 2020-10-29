32984488d4d6242d32f910c3dc5603e60c9b17a8
Guilherme Ribeiro <Gui>
2020-10-29 11:20:07 +0000

    test refactoring

--- tests/unit/user.spec.js
+++ tests/unit/user.spec.js
@@ -2,18 +2,24 @@ import User from '@/components/User.vue'
 import { mount } from '@vue/test-utils'
 describe('user component template', () => {
 
+    let component;
+
+    beforeEach(() => {
+        component = mount(User)
+    })
+
     it('should exists', () => {
-        const component = mount(User)
+        
         expect(component.exists())
     })
 
     it('should contain header', () => {
-        const component = mount(User)
+        
         expect(component.find('h1').text()).toBe('Welcome to the pub')
     })
 
     it('should contain input for user typing his/her name', () => {
-        const component = mount(User)
+        
         expect(component.find('input').exists())
     })
 })

f235f5b2f74552cff2e93ce3f9e886ecf74caf85
Guilherme Ribeiro <Gui>
2020-10-29 10:38:14 +0000

    added test for component

--- tests/unit/user.spec.js
+++ tests/unit/user.spec.js
@@ -1,1 +1,9 @@
 import User from '@/components/User.vue'
+import { mount } from '@vue/test-utils'
+describe('user component template', () => {
+
+    it('should exists', () => {
+        const component = mount(User)
+        expect(component.exists())
+    })
+})
